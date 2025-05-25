16626ef1e7214a54cf8e471ce35fe2abcd841da0
SaveTheTitanfall <prokopenkoi574@gmail.com>
2025-05-25 14:30:07 +0300

    dz2

--- para.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from math import sqrt
-import pandas as pd
-data=pd.Series(np.random.randint(10,20,10))
-data
-print(data)
-num=pd.DataFrame(np.random.randint(10,20,size=(9,15)),index=("ww","222","111","ww","222","111","ww","222","111"))
